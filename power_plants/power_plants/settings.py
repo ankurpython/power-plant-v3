13e178090c3e54026fa7b6651fb9ceb46b0b207f
Ankur Saurabh <imankursaurabh@outlook.com>
2021-11-15 07:18:35 +0000

    Update settings.py

--- power_plants/power_plants/settings.py
+++ power_plants/power_plants/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
+         'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
