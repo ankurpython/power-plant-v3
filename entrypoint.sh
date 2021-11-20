3a6e499fabaacaf3cf7eef60d13836c1d811dddf
Ankur Saurabh <imankursaurabh@outlook.com>
2021-11-20 19:09:02 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-python manage.py add_data
+python manage.py migrate
+python manage.py collectstatic
 
 gunicorn power_plants.wsgi:application --bind 0.0.0.0:8000
 
